ee3848878568e31773f0b62e93f4ff57233b15af
ayushchoudhary <a3506794@gmail.com>
2026-01-19 19:38:30 +0530

    client function Addfile

--- backend/internal/client/methods.go
+++ backend/internal/client/methods.go
@@ -1,8 +1,19 @@
 package client
 
-import(
+import (
+	"log"
 
+	"github.com/ayushchoudhary-3190/Distributed_file_system/internal/metaservice"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 
 
+func AddFile(newFile *metaservice.NewFile_Params) (uuid.UUID,string) {
+	fileID,err:= metaservice.CreateFile(newFile)
+	if err!=nil{
+		log.Fatal("failed to create metaservice entry")
+	}
+
+}
--- backend/internal/metaservice/service.go
+++ backend/internal/metaservice/service.go
@@ -1,47 +1,43 @@
 package metaservice
 
 import (
+	"github.com/ayushchoudhary-3190/Distributed_file_system/internal/client"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type Database struct {
-	db *gorm.DB
+
+type NewFile_Params struct{
+	newFile *File_info
+	db 		*gorm.DB
 }
 
-type NewFileEntry struct {
+type File_info struct{
 	FileName   string
 	OwnerID    string
 	ChunkCount int64
 	ChunkArray []string
 	FileSize   int64
 }
-func NewFileEntity(fileName string, ownerID string, chunkCount int64, chunkArray []string, fileSize int64) *NewFileEntry {
-	return &NewFileEntry{
-		FileName:   fileName,
-		OwnerID:    ownerID,
-		ChunkCount: chunkCount,
-		ChunkArray: chunkArray,
-		FileSize:   fileSize,
-	}
-}
+
+
 //  function to add a new file to the metaservice table
-func (DB *Database) CreateFile(newFile *NewFileEntry) (int64, error) {
-	var insertedID int64
-	err := DB.db.Transaction(func(tx *gorm.DB) error {
+func  CreateFile(newFileInfo *NewFile_Params) (uuid.UUID, error) {
+	fileID:= uuid.New()
+	err := newFileInfo.db.Transaction(func(tx *gorm.DB) error {
 		result := tx.Exec(`
-			INSERT INTO file_tables(file_name,ownerid,chunkcount,chunkarray,filesize) VALUES(?,?,?,?,?)
-		`, newFile.FileName, newFile.OwnerID, newFile.ChunkCount, newFile.ChunkArray, newFile.FileSize)
+			INSERT INTO file_tables(fileid,file_name,ownerid,chunkcount,chunkarray,filesize) VALUES(?,?,?,?,?,?)
+		`,fileID, newFileInfo.newFile.FileName, newFileInfo.newFile.OwnerID, newFileInfo.newFile.ChunkCount, newFileInfo.newFile.ChunkArray, newFileInfo.newFile.FileSize)
 		if result.Error != nil {
 			return result.Error
 		}
 
-		insertedID = result.RowsAffected
-		return nil
+		return result.Error
 	})
 	if err != nil {
-		return 0, err
+		return  uuid.Nil,err
 	}
-	return insertedID, nil
+	return fileID, nil
 }
 
 
